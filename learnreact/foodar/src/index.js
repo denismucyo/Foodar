2b3490baa8c923839e8201088d7676fbc7f70717
denismucyo <denismucyo88@gmail.com>
2021-12-10 19:23:04 -0600

    first commit

---
+++ learnreact/foodar/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Home from "./home";
+
+ReactDOM.render(
+    <Home/>,
+    document.getElementById('root')
+);
